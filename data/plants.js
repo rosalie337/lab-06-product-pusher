e9c069846e2248dfcd7b1e7a1e403939ec25de17
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-05 16:23:50 -0700

    completed plants.js

---
+++ data/plants.js
@@ -0,0 +1,94 @@
+const jade = {
+    id: 'jade',
+    name: 'Crassula ovata',
+    image: 'jade.png',
+    description: 'Jade—a popular houseplant that is native to South Africa—retains water in its round leaves',
+    classification: 'Pigmyweeds',
+    price: 20.00,
+    origin: 'South Africa'
+};
+
+const fiddle = {
+    id: 'fiddle-leaf fig',
+    name: 'Ficus lyrata',
+    image: 'fiddle.png',
+    description: 'These trendy trees have more than just lush foliage going for them. Their hardy disposition can adapt to most bright locations (minus direct sunlight).',
+    classification: 'Fig trees',
+    price: 90.00,
+    origin: 'Western Africa'
+};
+
+const chestnut = {
+    id: 'guiana chestnut',
+    name: 'Pachira aquatica',
+    image: 'chestnut.png',
+    description: 'JMore commonly known as money tree, Pachira aquatica frequently features a braided trunk.',
+    classification: 'Pachira',
+    price: 10.00,
+    origin: 'Central and South America'
+};
+
+
+const money = {
+    id: 'chinese money plant',
+    name: 'Pilea peperomioides',
+    image: 'money.png',
+    description: 'Here\'s another plant with fortuitous associations, although it also goes by the adorable nickname "Pancake Plant.',
+    classification: 'Clearweed',
+    price: 15.00,
+    origin: 'Southern China'
+};
+
+const calathea = {
+    id: 'calathea',
+    name: 'Calathea',
+    image: 'calathea.png',
+    description: '"Prayer plants" are grown for their foliage alone, and it\'s easy to see why. The purple, green, pink, and red leaves put on quite the show.',
+    classification: 'Arrowroots',
+    price: 25.00,
+    origin: 'South America'
+};
+
+const rubber = {
+    id: 'rubber plant',
+    name: 'Ficus elastica',
+    image: 'rubber.png',
+    description: 'Rubber trees can measure over 100 feet tall in their native Asia, but regular pruning can keep the ornamental variety in check.',
+    classification: 'Fig tress',
+    price: 15.00,
+    origin: 'Asia'
+};
+
+const bromeliad = {
+    id: 'bromeliad',
+    name: 'Bromeliaceae',
+    image: 'bromeliad.png',
+    description: 'Like the pineapple, the bromeliad belongs to the bromeliaceae family. This plant "lasts a long time," says Sharon Nejman, Senior Horticulturist at Chicago Botanic Garden.',
+    classification: 'Poales',
+    price: 20.00,
+    origin: 'West Africa'
+};
+
+const philodendron= {
+    id: 'philodendron',
+    name: 'Philodendron',
+    image: 'phil.png',
+    description: 'Hundreds of species of the large-leafed philodendron grow in the West Indies, Mexico, and Brazil.',
+    classification: 'Araceae',
+    price: 30.00,
+    origin: 'South America'
+};
+
+const plants = [
+    jade,
+    fiddle,
+    chestnut,
+    money-plant,
+    rubber,
+    calathea,
+    bromeliad,
+    philodendron,
+
+];
+
+export default plants;
