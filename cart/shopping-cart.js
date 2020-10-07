5ab19514e7c245ac591cfad4cdcfed57e7175000
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-06 17:27:07 -0700

    added code to shopping-cart.js with notes

--- cart/shopping-cart.js
+++ cart/shopping-cart.js
@@ -1,1 +1,19 @@
-import cart from '../data/plants.js'
+import cart from '../data/cart.js';
+import plants from '../data/plants.js';
+import { findById, orderTotal, toUSD } from '../common/utils.js';
+import createRow from './render-cart-items.js';
+
+const tbody = document.querySelector('tbody');
+const orderTotalCell = document.getElementById('order-total-cell'); // value is never read?
+
+for(let i = 0; cart.length; i++) {
+    
+    const cartItem = cart[i]; // will get the cart items 
+    const plant = findById(plants, cartItem.id); // this is suppose to give us the price and display name of the item, how?
+    const dom = createRow(cartItem, plant); 
+
+    tbody.appendChild(dom);
+}
+
+const total = orderTotal(cart, plants);
+orderTotal.textContent = toUSD(total);
